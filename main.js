7a764cd38789f443e85e365306a46c386a7f8c82
Roiyals <194830982+Roiyals@users.noreply.github.com>
2025-11-13 16:41:41 -0500

    create function to clear display and update values
    Created a function that clears the display instead
    of nesting it in the displayStuff() function.
    
    Created an object that stores the parameters for
    the operate() function. In addition, created a
    function that updates the values of said object,
    still in WIP.

--- main.js
+++ main.js
@@ -23,7 +23,8 @@ const display = document.querySelector(".display");
 const numberButtons = document.querySelectorAll(".numbers-section button");
 numberButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    displayStuff(button.textContent);
+    displayStuff(+button.textContent);
+    updateCalc(+button.textContent);
   });
 });
 
@@ -32,17 +33,57 @@ const operatorButtons = document.querySelectorAll(
 );
 operatorButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    displayStuff(button.textContent);
+    if (button.textContent === "C") {
+      clearDisplay(button.textContent);
+    } else {
+      updateCalc(button.textContent);
+    }
   });
 });
 
+let calc = {
+  firstNumber: "",
+  secondNumber: "",
+  operator: "",
+  equalTo: "",
+};
+
+function updateCalc(button) {
+  function number() {
+    let nodeList = document.querySelectorAll("div p");
+    let numArr = [...nodeList].map((p) => +p.textContent);
+
+    return numArr.join("");
+  }
+
+  if (calc.firstNumber && calc.operator) {
+    calc.secondNumber = number();
+  } else if (typeof button !== "number" && calc.firstNumber) {
+    calc.operator = button;
+  } else if (typeof button === "number" && calc.secondNumber === "") {
+    calc.firstNumber = number();
+  }
+
+  console.log(calc);
+}
+
 function displayStuff(button) {
+  const p = document.createElement("p");
+  p.style.display = "inline-block";
+  p.textContent = button;
+  display.appendChild(p);
+}
+
+function clearDisplay(button) {
+  // Full reset
   if (button === "C") {
     display.textContent = "";
+    calc.firstNumber = "";
+    calc.secondNumber = "";
+    calc.operator = "";
     return;
   }
 
-  const p = document.createElement("p");
-  p.textContent = button;
-  display.appendChild(p);
+  // Clears display everytime seconNumber is updated
+  display.textContent = "";
 }
